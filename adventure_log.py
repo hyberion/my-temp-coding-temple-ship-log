a0223753d290d7ccabfb3b2511f4fd41d1b39790
hyberion <hyberion@gmail.com>
2024-09-07 22:42:16 -0400

    All hail the Omnisiah

---
+++ adventure_log.py
@@ -0,0 +1,5 @@
+def greet_coding_temple():
+    print("Are you rady kids?  I can't hear you!  Are you ready for nautical nonesense?  Can you say 'The emperor protects':")
+
+if __name__ == "__main__":
+    greet_coding_temple()
